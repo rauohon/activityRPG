3d045901d28cc3d2f9be5705fc392e2db7efd0c3
rauohon <rnjdejr@gmail.com>
2017-09-28 19:25:15 +0900

    최초 커밋 입니다.

---
+++ src/com/activityRPG/dao/IMBupdate.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.activityRPG.dao;
+
+/**
+ * @클래스명 : IMBupdate
+ * @작성자 : 신태휘
+ * @작성일 : 2017. 9. 28.
+ * @설명 : 
+ * @수정이력 - 수정일, 수정자, 수정내용
+ */
+public interface IMBupdate {
+
+}
